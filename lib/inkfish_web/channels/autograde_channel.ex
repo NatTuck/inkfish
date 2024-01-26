59adbe004980aac6660365892d1a1417421a841c
Nat Tuck <nat@ferrus.net>
2024-01-26 11:57:39 -0500

    duplicates in autograde channel?

--- lib/inkfish_web/channels/autograde_channel.ex
+++ lib/inkfish_web/channels/autograde_channel.ex
@@ -9,7 +9,7 @@ defmodule InkfishWeb.AutogradeChannel do
         socket = socket
         |> assign(:uuid, uuid)
 
-	Process.send_after(self(), :open, 1)
+	    Process.send_after(self(), :open, 1)
 
         {:ok, %{blocks: []}, socket}
       failure ->
@@ -22,26 +22,26 @@ defmodule InkfishWeb.AutogradeChannel do
     uuid = socket.assigns[:uuid]
     case Inkfish.Itty.open(uuid) do
       {:ok, %{blocks: blocks, done: done}} ->
-	Enum.each blocks, fn block ->
-	  push(socket, "block", block)
-	end
+	    Enum.each blocks, fn block ->
+	      push(socket, "block", block)
+	    end
 
         if done do
           Process.send_after(self(), {:done, uuid}, 1)
         end
       {:error, _} ->
-	grade = Grades.get_grade_by_log_uuid(uuid)
-	|> Grades.preload_sub_and_upload()
+	    grade = Grades.get_grade_by_log_uuid(uuid)
+	    |> Grades.preload_sub_and_upload()
 	
-	if grade do
-	  log = Grades.Grade.get_log(grade)
-	  push(socket, "block", %{seq: 10, stream: :err, text: "itty missing\n" })
-	  push(socket, "block", %{seq: 11, stream: :out, text: Jason.encode!(log)})
-	else
-	  push(socket, "block", %{seq: 10, stream: :err, text: "itty missing\n" })
-	end
+	    if grade do
+	      log = Grades.Grade.get_log(grade)
+	      push(socket, "block", %{seq: 10, stream: :err, text: "itty missing\n" })
+	      push(socket, "block", %{seq: 11, stream: :out, text: Jason.encode!(log)})
+	    else
+	      push(socket, "block", %{seq: 10, stream: :err, text: "itty missing\n" })
+	    end
 
-	Process.send_after(self(), {:done, uuid}, 1)
+	    Process.send_after(self(), {:done, uuid}, 1)
     end
 
     {:noreply, socket}
@@ -68,8 +68,10 @@ defmodule InkfishWeb.AutogradeChannel do
     {:noreply, socket}
   end
 
-  def handle_info({:block, _uuid, item}, socket) do
-    #IO.inspect {:block, item}
+  def handle_info({:block, uuid, item}, socket) do
+    if uuid != socket.assigns[:uuid] do
+      IO.inspect({:uuid_mismatch, uuid, socket.assigns[:uuid]})
+    end
     push(socket, "block", item)
     {:noreply, socket}
   end
@@ -77,6 +79,7 @@ defmodule InkfishWeb.AutogradeChannel do
   def handle_info({:done, uuid}, socket) do
     #IO.inspect {:done, uuid}
     push(socket, "done", %{uuid: uuid})
+    Inkfish.Itty.close(uuid)
     {:noreply, socket}
   end
 end
