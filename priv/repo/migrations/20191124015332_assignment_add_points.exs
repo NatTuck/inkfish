d8448e4ddfec4fce9cdde6b4d69a26aad125e8c2
Nat Tuck <nat@ferrus.net>
2024-02-28 14:32:01 -0600

    skip obsolete migration steps

--- priv/repo/migrations/20191124015332_assignment_add_points.exs
+++ priv/repo/migrations/20191124015332_assignment_add_points.exs
@@ -1,19 +1,10 @@
 defmodule Inkfish.Repo.Migrations.AssignmentAddPoints do
   use Ecto.Migration
 
-  alias Inkfish.Repo
-  alias Inkfish.Assignments
-  alias Inkfish.Assignments.Assignment
-
   def up do
     alter table("assignments") do
       add :points, :decimal, null: false, default: "0.0"
     end
-
-    flush()
-
-    Repo.all(Assignment)
-    |> Enum.each(&Assignments.update_assignment_points!/1)
   end
 
   def down do
