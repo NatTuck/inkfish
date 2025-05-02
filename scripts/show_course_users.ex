0b949ff3a359427e9012b920cdc9be39879051ce
Nat Tuck <nat@ferrus.net>
2025-05-02 18:43:16 -0400

    add script to dump users and secrets for a course

---
+++ scripts/show_course_users.ex
@@ -0,0 +1,24 @@
+defmodule ShowCourseUsers do
+  alias Inkfish.Users
+  alias Inkfish.Courses
+  alias Inkfish.Repo
+
+  def main(co_id) do
+    course = Courses.get_course!(co_id)
+    |> Repo.preload(regs: [:user])
+
+    users = Enum.map(course.regs, &(&1.user))
+   
+    Enum.each users, fn user ->
+      login = Regex.replace(~r/@.*$/, user.email, "")
+      IO.puts("#{login}\t#{user.secret}")
+    end
+  end
+end
+
+argv = System.argv()
+
+[co_id] = argv
+{co_id, _} = Integer.parse(co_id)
+
+ShowCourseUsers.main(co_id)
