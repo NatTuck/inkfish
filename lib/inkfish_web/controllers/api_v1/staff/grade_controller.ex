2360551dac4ed60afbc9aa93c85e3b0cded48b14
Nat Tuck <nat@ferrus.net>
2025-08-01 22:46:04 -0400

    refactor: update grade controller to use sub_id and put_grade_with_comments

--- lib/inkfish_web/controllers/api_v1/staff/grade_controller.ex
+++ lib/inkfish_web/controllers/api_v1/staff/grade_controller.ex
@@ -3,10 +3,13 @@ defmodule InkfishWeb.ApiV1.Staff.GradeController do
 
   alias Inkfish.Grades
   alias Inkfish.Grades.Grade
+  alias Inkfish.Subs
 
   action_fallback InkfishWeb.FallbackController
 
-  plug InkfishWeb.Plugs.RequireApiUser
+  alias InkfishWeb.Plugs
+
+  plug Plugs.RequireApiUser
 
   plug Plugs.FetchItem,
        [sub: "id"]
@@ -18,13 +21,16 @@ defmodule InkfishWeb.ApiV1.Staff.GradeController do
 
   plug Plugs.RequireReg, staff: true
 
-  def index(conn, _params) do
-    grades = Grades.list_grades()
-    render(conn, :index, grades: grades)
+  def index(conn, %{"sub_id" => sub_id}) do
+    sub = Subs.get_sub_with_grades!(sub_id)
+    render(conn, :index, grades: sub.grades)
   end
 
   def create(conn, %{"grade" => grade_params}) do
-    with {:ok, %Grade{} = grade} <- Grades.create_grade(grade_params) do
+    user = conn[:current_user]
+
+    with {:ok, %Grade{} = grade} <-
+           Grades.put_grade_with_comments(grade_params, user) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", ~p"/api/api_v1/staff/grades/#{grade}")
