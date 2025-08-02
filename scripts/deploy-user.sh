76c7f69d9a9bbe77a31ef059648d9ca304f18294
Nat Tuck <nat@ferrus.net>
2025-08-02 20:43:23 +0000

    fix deploy script

--- scripts/deploy-user.sh
+++ scripts/deploy-user.sh
@@ -11,6 +11,7 @@ export MIX_ENV=prod
 export PORT=4080
 export DATABASE_URL=$(cat ~/.config/inkfish/db_url)
 export SECRET_KEY_BASE=$(cat ~/.config/inkfish/key_base)
+export MAILJET_KEY=$(cat ~/.config/inkfish/mailjet_key)
 
 systemctl --user stop inkfish
 
