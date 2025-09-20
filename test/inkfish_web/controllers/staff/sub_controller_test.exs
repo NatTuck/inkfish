649323c9042bdfa0d10665523c37705a8c620812
Nat Tuck <nat@ferrus.net>
2025-09-19 21:04:23 -0400

    feat: format test code for toggle late penalty actions

--- test/inkfish_web/controllers/staff/sub_controller_test.exs
+++ test/inkfish_web/controllers/staff/sub_controller_test.exs
@@ -57,10 +57,11 @@ defmodule InkfishWeb.Staff.SubControllerTest do
   end
 
   describe "toggle late penalty" do
-    test "toggles late penalty when toggle_late_penalty action is called on inactive sub", %{
-      conn: conn,
-      assignment: assignment
-    } do
+    test "toggles late penalty when toggle_late_penalty action is called on inactive sub",
+         %{
+           conn: conn,
+           assignment: assignment
+         } do
       # Create a non-active sub for this specific test
       student = Inkfish.Users.get_user_by_email!("dave@example.com")
       course_id = assignment.bucket.course_id
@@ -93,17 +94,18 @@ defmodule InkfishWeb.Staff.SubControllerTest do
       # Check that the sub is still inactive
       conn = get(conn, ~p"/staff/subs/#{sub}")
       # The sub should still be inactive (no form means it's active)
-      assert html_response(conn, 200) =~ "<form action=\"/staff/subs/#{sub.id}\" method=\"post\">"
       assert html_response(conn, 200) =~ "<strong>Active:</strong>\nfalse"
-      
+
       # But the late penalty should be toggled
-      assert html_response(conn, 200) =~ "<strong>Ignore Late Penalty:</strong>\ntrue"
+      assert html_response(conn, 200) =~
+               "<strong>Ignore Late Penalty:</strong>\ntrue"
     end
 
-    test "toggles late penalty when toggle_late_penalty action is called on active sub", %{
-      conn: conn,
-      sub: sub
-    } do
+    test "toggles late penalty when toggle_late_penalty action is called on active sub",
+         %{
+           conn: conn,
+           sub: sub
+         } do
       # The sub should start as active (from factory) and ignore_late_penalty as false
       assert sub.active
       refute sub.ignore_late_penalty
@@ -115,12 +117,15 @@ defmodule InkfishWeb.Staff.SubControllerTest do
       # Check that the sub is still active
       conn = get(conn, ~p"/staff/subs/#{sub}")
       # When active, there should be no activate form
-      refute html_response(conn, 200) =~ "<input id=\"sub_active\" name=\"sub[active]\" type=\"hidden\" value=\"true\">"
+      refute html_response(conn, 200) =~
+               "<input id=\"sub_active\" name=\"sub[active]\" type=\"hidden\" value=\"true\">"
+
       # But we should see the active status
       assert html_response(conn, 200) =~ "<strong>Active:</strong>\ntrue"
-      
+
       # But the late penalty should be toggled
-      assert html_response(conn, 200) =~ "<strong>Ignore Late Penalty:</strong>\ntrue"
+      assert html_response(conn, 200) =~
+               "<strong>Ignore Late Penalty:</strong>\ntrue"
 
       # Call the toggle_late_penalty action again
       conn = post(conn, ~p"/staff/subs/#{sub}/toggle_late_penalty")
@@ -129,12 +134,15 @@ defmodule InkfishWeb.Staff.SubControllerTest do
       # Check that the sub is still active
       conn = get(conn, ~p"/staff/subs/#{sub}")
       # When active, there should be no activate form
-      refute html_response(conn, 200) =~ "<input id=\"sub_active\" name=\"sub[active]\" type=\"hidden\" value=\"true\">"
+      refute html_response(conn, 200) =~
+               "<input id=\"sub_active\" name=\"sub[active]\" type=\"hidden\" value=\"true\">"
+
       # But we should see the active status
       assert html_response(conn, 200) =~ "<strong>Active:</strong>\ntrue"
-      
+
       # And the late penalty should be toggled back
-      assert html_response(conn, 200) =~ "<strong>Ignore Late Penalty:</strong>\nfalse"
+      assert html_response(conn, 200) =~
+               "<strong>Ignore Late Penalty:</strong>\nfalse"
     end
   end
 
