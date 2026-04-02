714e19d989e5014909eb6f64acfeab5bf298849c
Nat Tuck <nat@ferrus.net>
2026-04-02 14:08:31 -0400

    fix syntax bug again

--- test/inkfish_web/controllers/staff/grade_controller_test.exs
+++ test/inkfish_web/controllers/staff/grade_controller_test.exs
@@ -2,6 +2,8 @@ defmodule InkfishWeb.Staff.GradeControllerTest do
   use InkfishWeb.ConnCase
   import Inkfish.Factory
 
+  alias Inkfish.Uploads.Upload
+
   setup %{conn: conn} do
     %{staff: staff, sub: sub, grade: grade} = stock_course()
     conn = login(conn, staff)
@@ -12,6 +14,26 @@ defmodule InkfishWeb.Staff.GradeControllerTest do
     test "shows grade", %{conn: conn, grade: grade} do
       conn = get(conn, ~p"/staff/grades/#{grade}")
       assert html_response(conn, 200) =~ "Show Grade"
+    end
+
+    test "javascript data is properly escaped", %{conn: conn, grade: grade} do
+      upload = grade.sub.upload
+      unpacked = Upload.unpacked_path(upload)
+      File.mkdir_p!(unpacked)
+
+      File.write!(Path.join(unpacked, "test.js"), "var x = \"hello\";")
+      File.write!(Path.join(unpacked, "draw.js/"), "content with \"quotes\"")
+
+      File.write!(
+        Path.join(unpacked, "file\nwith\nnewlines.txt"),
+        "line1\nline2"
+      )
+
+      conn = get(conn, ~p"/staff/grades/#{grade}")
+      html = html_response(conn, 200)
+
+      refute html =~ "window.code_view_data",
+             "Show page should not have code viewer for this grade type"
     end
   end
 
@@ -44,6 +66,37 @@ defmodule InkfishWeb.Staff.GradeControllerTest do
     test "renders form for editing chosen grade", %{conn: conn, grade: grade} do
       conn = get(conn, ~p"/staff/grades/#{grade}/edit")
       assert html_response(conn, 200) =~ "Edit Grade"
+    end
+
+    test "javascript data is properly escaped for edit", %{
+      conn: conn,
+      grade: grade
+    } do
+      upload = grade.sub.upload
+      unpacked = Upload.unpacked_path(upload)
+      File.mkdir_p!(unpacked)
+
+      File.write!(Path.join(unpacked, "test.js"), "var x = \"hello\";")
+
+      File.write!(
+        Path.join(unpacked, "problematic.txt"),
+        "Content with \"quotes\" and\nnewlines"
+      )
+
+      conn = get(conn, ~p"/staff/grades/#{grade}/edit")
+      html = html_response(conn, 200)
+
+      assert html =~ "window.code_view_data = JSON.parse(",
+             "Should use JSON.parse for proper escaping"
+
+      assert html =~ "test.js",
+             "Should contain file name test.js"
+
+      assert html =~ "problematic.txt",
+             "Should contain file name"
+
+      refute html =~ ~s("test.js"),
+             "Should not have unescaped quotes in file names"
     end
   end
 end
